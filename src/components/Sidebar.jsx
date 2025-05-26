21c13c31864f000fdd25e5ab9dc2a76980508748
DBStirling <davidstirling17@gmail.com>
2025-05-26 15:46:29 -0700

    temp-fix for sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,26 +13,26 @@ const Sidebar = ({ node, onClose }) => {
             {/* Optional: design background element */}
             <div className="relative py-[120px] flex flex-col gap-10">
             {/* Header with name */}
-            <div className={`border-l-[20px]`} style={{ borderColor: node.color || '#868686' }}>
+            <div className={`border-l-[6px]`} style={{ borderColor: node.color || '#868686' }}>
                 <div className="pl-[64px] flex flex-col gap-[4px] text-[#c9c9c9] w-full font-['Space_Grotesk'] leading-none font-light uppercase">
-                        <div className="ml-[3px] text-[24px]">{node.fullName?.split(' ')[0]}
+                        <div className="ml-[3px] text-[20px]">{node.fullName?.split(' ')[0]}
                         </div>
-                        <div className="text-[64px]">
+                        <div className="text-[40px]">
                             {node.fullName?.split(' ').slice(1).join(' ') || ''}
                         </div>
                     </div> 
             </div>
 
-            <div className="pl-[86px] flex flex-col gap-5 font-['Space_Grotesk']">
+            <div className="pl-[74px] flex flex-col gap-5 font-['Space_Grotesk']">
                 {/* Website */}
                 {node.website && (
                 <div className="flex flex-col">
-                    <div className="text-[20px] text-[#3e3e3e]">WEBSITE</div>
+                    <div className="text-[14px] text-[#3e3e3e]">WEBSITE</div>
                     <a
                     href={node.website}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-[32px] text-[#c9c9c9] font-light underline break-all"
+                    className="text-[20px] text-[#c9c9c9] font-light underline break-all"
                     >
                     {node.website}
                     </a>
@@ -47,8 +47,8 @@ const Sidebar = ({ node, onClose }) => {
                 {Object.entries(node.skills || {}).map(([label, value], idx) =>
                     value ? (
                     <div key={idx} className="flex flex-col w-fit">
-                        <div className="text-[20px] text-[#3e3e3e] font-light">{label.toUpperCase()}</div>
-                        <div className="text-[32px]  text-[#c9c9c9] font-light">{value}</div>
+                        <div className="text-[14px] text-[#3e3e3e] font-light">{label.toUpperCase()}</div>
+                        <div className="text-[20px]  text-[#c9c9c9] font-light">{value}</div>
                     </div>
                     ) : null
                 )}
@@ -60,8 +60,8 @@ const Sidebar = ({ node, onClose }) => {
                 {/* Bio */}
                 {node.bio && (
                 <div className="flex flex-col gap-2">
-                    <div className="text-[20px] text-[#3e3e3e] uppercase tracking-wider">Bio</div>
-                    <div className="text-[32px] leading-[28px] text-[#c9c9c9] font-light whitespace-pre-line break-words">
+                    <div className="text-[14px] text-[#3e3e3e] uppercase tracking-wider">Bio</div>
+                    <div className="text-[20px] leading-[28px] text-[#c9c9c9] font-light whitespace-pre-line break-words">
                     {node.bio}
                     </div>
                 </div>
